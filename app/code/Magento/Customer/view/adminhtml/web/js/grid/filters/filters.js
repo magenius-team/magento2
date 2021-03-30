951e15247ebc8771554def87c22646087f5a4f56
Denis Kopylov <dkopylov@magenius.team>
2021-03-30 15:47:10 +0300

    [Customer] Use bookmarks without storing filters

--- app/code/Magento/Customer/view/adminhtml/web/js/grid/filters/filters.js
+++ app/code/Magento/Customer/view/adminhtml/web/js/grid/filters/filters.js
@@ -3,9 +3,6 @@
  * See COPYING.txt for license details.
  */
 
-/**
-* @deprecated
-*/
 define([
     'Magento_Ui/js/grid/filters/filters'
 ], function (Filters) {
@@ -13,10 +10,8 @@ define([
 
     return Filters.extend({
         defaults: {
-            chipsConfig: {
-                name: '${ $.name }_chips',
-                provider: '${ $.chipsConfig.name }',
-                component: 'Magento_Customer/js/grid/filters/chips'
+            statefull: {
+                applied: false
             }
         }
     });
